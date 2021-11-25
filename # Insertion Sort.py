5798183b6368e139ace6db616e7403ebc7a8cc92
Coologic <78857657+Devukth@users.noreply.github.com>
2021-11-25 18:36:19 +0530

    Add files via upload

---
+++ # Insertion Sort.py
@@ -0,0 +1,15 @@
+# Insertion Sort
+def sort(arr):
+    arr_edit = arr
+    for i in range(1, len(arr)):
+        temp = arr_edit[i]
+
+        j = i - 1
+        while j >= 0 and temp < arr_edit[j]:
+            arr_edit[j + 1] = arr_edit[j]
+            j -= 1
+        arr_edit[j + 1] = temp
+    return arr_edit
+
+arr = [1, 5, 2, 4, 3]
+print(sort(arr))
